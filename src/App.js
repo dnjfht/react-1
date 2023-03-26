1ebbddef393c8f21131ef01f08a4d317515e5523
Yoo Seungmin <dnjfht@naver.com>
2023-03-26 19:47:30 +0900

    react study

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  let name = "min";
+
+  // return 안에서 if문 선언 불가
+  // if문 대신 삼항연산자를 쓸 수 있음
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div
+      style={{
+        width: "360px",
+        padding: "1.6rem",
+        boxSizing: "border-radius",
+        border: "1px solid #ccc",
+        borderRadius: "0.8rem",
+        textAlign: "center",
+        margin: "4rem auto",
+        backgroundColor: "rgba(255,0,108,0.8)",
+      }}
+    >
+      <div className="title" style={{ borderBottom: "1px solid #ccc" }}>
+        <h1 style={{ fontSize: "1.6rem", color: "#fff", fontWeight: "500" }}>
+          안녕하세요!
+        </h1>
+      </div>
+
+      <div
+        className="input_name"
+        style={{
+          textAlign: "left",
+          padding: "1rem 0",
+          boxSizing: "border-box",
+        }}
+      >
+        <p style={{ fontSize: "0.8rem", color: "#fff" }}>
+          이름을 입력해주세요.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input
+          type="text"
+          style={{
+            width: "100%",
+            height: "46px",
+            padding: "1rem",
+            boxSizing: "border-box",
+            border: "1px solid #fff",
+            borderRadius: "0.4rem",
+            outline: "none",
+            backgroundColor: "transparent",
+          }}
+        />
+      </div>
     </div>
   );
 }
